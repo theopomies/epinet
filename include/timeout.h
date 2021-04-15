2720d9c752cc882ac6e71965f71229d2dcb11fe0
Ken Kakura <theo.pomies@epitech.eu>
2021-04-15 20:22:53 +0200

    ADD: timeout.h

---
+++ include/timeout.h
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2021
+** Epinet
+** File description:
+** timeout
+*/
+
+#ifndef TIMEOUT_H_
+#define TIMEOUT_H_
+
+#define NO_TIMEOUT -1
+
+typedef int time_t
+
+#endif /* !TIMEOUT_H_ */
