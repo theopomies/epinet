a5d046e23f04ad8898478a79b3201fc781c6c819
Ken Kakura <theo.pomies@epitech.eu>
2021-04-18 11:43:20 +0200

    ADD: Socket.h doc

--- include/socket.h
+++ include/socket.h
@@ -39,11 +39,52 @@ typedef struct
     bool is_blocking;
 } socket_t;
 
+////////////////////////////////////////////////////////////////////////////////
+/// \brief Creates a socket of type \a type.
+///
+/// Allocates and calls socket().
+///
+/// \param type The type of socket to create. (SOCKET_TCP or SOCKET_UDP)
+///
+/// \return The created socket on success, NULL on error and epinet_perror set.
+////////////////////////////////////////////////////////////////////////////////
 socket_t *socket_create(socket_type_t type);
+
+////////////////////////////////////////////////////////////////////////////////
+/// \brief Creates an empty socket (without actual socket) of type \a type.
+///
+/// It allocates the socket but doesnt call socket(). Used to accept().
+///
+/// \param type The type of socket to create. (SOCKET_TCP or SOCKET_UDP)
+///
+/// \return The created socket on success, NULL on error and epinet_perror set.
+////////////////////////////////////////////////////////////////////////////////
 socket_t *socket_create_empty(socket_type_t type);
+
+////////////////////////////////////////////////////////////////////////////////
+/// \brief Destroys the \a socket object. Closes it and frees it.
+///
+/// \param type The socket object to destroy.
+////////////////////////////////////////////////////////////////////////////////
 void socket_destroy(socket_t *sock);
 
+////////////////////////////////////////////////////////////////////////////////
+/// \brief Sets the blocking-ness of a socket.
+///
+/// TODO Real implementation for outside epitech
+///
+/// \param sock Socket of which to set the blocking-ness.
+/// \param blocking Boolean of blocking state to set
+////////////////////////////////////////////////////////////////////////////////
 void socket_set_blocking(socket_t *sock, bool blocking);
+
+////////////////////////////////////////////////////////////////////////////////
+/// \brief Gets the blocking-ness of a socket
+///
+/// TODO Real implementation for outside epitech
+///
+/// \param sock Socket of which to get the blocking-ness.
+////////////////////////////////////////////////////////////////////////////////
 bool socket_is_blocking(socket_t *sock);
 
 #endif /* !SOCKET_H_ */
