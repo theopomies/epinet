80eda3042c5939c34cae65b12432189b322da701
Ken Kakura <theo.pomies@epitech.eu>
2021-04-18 11:08:58 +0200

    ADD: Header to include the lib

---
+++ include/epinet.h
@@ -0,0 +1,22 @@
+/*
+** EPITECH PROJECT, 2021
+** epinet
+** File description:
+** epinet
+*/
+
+#ifndef EPINET_H_
+#define EPINET_H_
+
+#include <error.h>
+#include <ftp.h>
+#include <http.h>
+#include <ip_address.h>
+#include <packet.h>
+#include <socket.h>
+#include <socket_selector.h>
+#include <tcp_listener.h>
+#include <tcp_socket.h>
+#include <udp_socket.h>
+
+#endif /* !EPINET_H_ */
