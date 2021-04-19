583ee356f256436f1a45bb766755dda10b53120d
Ken Kakura <theo.pomies@epitech.eu>
2021-04-19 15:11:27 +0200

    FIX: coding style ip_address/from_bytes

--- src/ip_address/from_bytes.c
+++ src/ip_address/from_bytes.c
@@ -10,5 +10,6 @@
 ip_address_t ip_address_from_bytes(
     uint8_t byte0, uint8_t byte1, uint8_t byte2, uint8_t byte3)
 {
-    return ip_address_from_integer((byte0 << 24) | (byte1 << 16) | (byte2 << 8) | byte3);
+    return ip_address_from_integer(
+        (byte0 << 24) | (byte1 << 16) | (byte2 << 8) | byte3);
 }
